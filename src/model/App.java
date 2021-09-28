36f000a33f4d08bbe1ed5e0f30f7a80e46b7daee
JuanDavidBallesteros <juan.ballesteros@u.icesi.edu.co>
2021-09-27 23:28:33 -0500

    Warming clean

--- src/model/App.java
+++ src/model/App.java
@@ -1,10 +1,8 @@
 package model;
 
 import java.io.BufferedReader;
-import java.io.BufferedWriter;
 import java.io.FileReader;
 import java.io.IOException;
-import java.io.OutputStreamWriter;
 import java.util.ArrayList;
 import java.util.Comparator;
 
